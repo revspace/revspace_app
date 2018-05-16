5db667a3c2758a248792428ffd3cc06eba9ba1a6
Peetz0r <github@haas-en-berg.nl>
2018-05-16 16:56:08 +0200

    Add flutter_secure_storage dependency
    I don't know yet how this works

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,20 @@ import 'dart:typed_data';
 import 'dart:ui' as ui show instantiateImageCodec, Codec;
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:image_picker/image_picker.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() => runApp(new RevspaceApp());
 
 List<RevImage> images = new List<RevImage>();
 SelectPhotosState selectPhotosState;
+final FlutterSecureStorage secureStorage = new FlutterSecureStorage();
 
 class RevspaceApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -270,61 +272,67 @@ class SelectPhotosState extends State<SelectPhotos> {
 
   void _onSettingsButtonPressed() {
     Navigator.of(context).push(
-        new MaterialPageRoute(
-            builder: (context) {
-              return new Scaffold(
-                  appBar: new AppBar(
-                    title: new Text('RevSpace Settings'),
-                  ),
-                  body: new SingleChildScrollView(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const SizedBox(height: 24.0),
-                          new Row(
-                              children: [
-                                const Expanded(
-                                  child: const Text('This account should be your revSpace Wiki account.\n\n'
-                                      'If you don\'t already have a wiki account, please contact a board member.'),
-                                ),
-                                new FloatingActionButton(
-                                    child: const Icon(Icons.email),
-                                    tooltip: 'Send email to board',
-                                    mini: true,
-                                    onPressed: () {
-                                      launch('mailto:board@revspace.nl?subject=Nieuw wiki account&body='
-                                          'Beste Bestuur,\n\n'
-                                          'Graag zou ik een account krijgen voor de RevSpace Wiki.\n\n'
-                                          'Mijn nickname is: \n\n'
-                                          'Geautomatiseerde groeten,\n\n'
-                                          'De RevSpace App');
-                                    }),
-                              ]),
-                          const SizedBox(height: 24.0),
-                          new TextFormField(
-                            decoration: const InputDecoration(
-                              border: const UnderlineInputBorder(),
-                              filled: true,
-                              hintText: 'RevSpace Wiki username',
-                              labelText: 'Username',
-                            ),
-                            onSaved: (String value) {
-                              print('Name: $value');
-                            },
+      new MaterialPageRoute(
+          builder: (context) {
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text('RevSpace Settings'),
+              ),
+              body: new SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: new Form(
+                  child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const SizedBox(height: 24.0),
+                        new Row(
+                            children: [
+                              const Expanded(
+                                child: const Text('This account should be your revSpace Wiki account.\n\n'
+                                    'If you don\'t already have a wiki account, please contact a board member.'),
+                              ),
+                              new FloatingActionButton(
+                                  child: const Icon(Icons.email),
+                                  tooltip: 'Send email to board',
+                                  mini: true,
+                                  onPressed: () {
+                                    launch('mailto:board@revspace.nl?subject=Nieuw wiki account&body='
+                                        'Beste Bestuur,\n\n'
+                                        'Graag zou ik een account krijgen voor de RevSpace Wiki.\n\n'
+                                        'Mijn nickname is: \n\n'
+                                        'Geautomatiseerde groeten,\n\n'
+                                        'De RevSpace App');
+                                  }),
+                            ]),
+                        const SizedBox(height: 24.0),
+                        new TextFormField(
+                          decoration: const InputDecoration(
+                            border: const UnderlineInputBorder(),
+                            filled: true,
+                            hintText: 'RevSpace Wiki username',
+                            labelText: 'Username',
                           ),
-                          const SizedBox(height: 24.0),
-                          new PasswordField(
-                            hintText: 'RevSpace Wiki password',
-                            labelText: 'Password',
-                            onSaved: (String value) {
-                              print('Password: $value');
-                            },
-                          ),
-                        ]),
-                  )
-              );
-            })
+                          onSaved: (String value) {
+                            print('Name: $value');
+                          },
+                        ),
+                        const SizedBox(height: 24.0),
+                        new PasswordField(
+                          hintText: 'RevSpace Wiki password',
+                          labelText: 'Password',
+                          onSaved: (String value) {
+                            print('Password: $value');
+                          },
+                        ),
+                        const SizedBox(height: 24.0),
+                      ]),
+                  onChanged: () {
+                    print('form changed');
+                  },
+                ),
+              ),
+            );
+          }),
     );
   }
 
