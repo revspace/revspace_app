c32579e537ae7a2b76ec72add694a435b5735bad
Peetz0r <github@haas-en-berg.nl>
2018-05-16 15:25:19 +0200

    Use top bar for settings button Use bottom bar for add buttons, Use Floating Action Button for sending
    Implement basic settings screen (does not actually set things yet)

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui show instantiateImageCodec, Codec;
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
 import 'package:image_picker/image_picker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(new RevspaceApp());
 
@@ -14,13 +18,64 @@ SelectPhotosState selectPhotosState;
 class RevspaceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return
+    return new MaterialApp(
+      title: 'RevSpace App',
+      theme: new ThemeData(primarySwatch: Colors.lightGreen),
+      home: new SelectPhotos(),
+    );
+  }
+}
 
-      new MaterialApp(
-        title: 'RevSpace App',
-        theme: new ThemeData(primarySwatch: Colors.lightGreen),
-        home: new SelectPhotos(),
-      );
+class PasswordField extends StatefulWidget {
+  const PasswordField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _PasswordFieldState createState() => new _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return new TextFormField(
+      key: widget.fieldKey,
+      obscureText: _obscureText,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: new InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: new GestureDetector(
+          onTap: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+          child: new Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+        ),
+      ),
+    );
   }
 }
 
@@ -95,24 +150,14 @@ class SelectPhotosState extends State<SelectPhotos> {
         title: new Text('Select Photos'),
         actions: [
           new IconButton(
-            onPressed: () => _onImageButtonPressed(ImageSource.gallery),
-            tooltip: 'Add photo from gallery',
-            icon: new Icon(Icons.add_photo_alternate),
-          ),
-          new IconButton(
-            onPressed: () => _onImageButtonPressed(ImageSource.camera),
-            tooltip: 'Add photo from camera',
-            icon: new Icon(Icons.add_a_photo),
-          ),
-          new IconButton(
-            onPressed: () => _onSendButtonPressed(),
-            tooltip: 'Send to RevSpace wiki',
-            color: (images.length > 0) ? Colors.black : Colors.grey,
-            icon: new Icon(Icons.send),
+            onPressed: () => _onSettingsButtonPressed(),
+            tooltip: 'Settings',
+            icon: new Icon(Icons.settings),
           ),
         ],
       ),
       body: new ListView.builder(
+          padding: const EdgeInsets.only(bottom: 32.0),
           itemCount: images.length,
           itemBuilder: (context, index) {
             return new Card(
@@ -138,48 +183,75 @@ class SelectPhotosState extends State<SelectPhotos> {
                       maxLines: 3,
                     ),
                     new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        new IconButton(
-                          iconSize: 32.0,
-                          onPressed: () {
-                            setState(() {
-                              images[index].rotation++;
-                            });
-                          },
-                          icon: new Icon(Icons.rotate_right, color: Theme
-                              .of(context)
-                              .primaryColor),
-                        ),
-                        new IconButton(
-                          iconSize: 32.0,
-                          onPressed: () {
-                            setState(() {
-                              images[index].rotation--;
-                            });
-                          },
-                          icon: new Icon(Icons.rotate_left, color: Theme
-                              .of(context)
-                              .primaryColor),
-                        ),
-                        new IconButton(
-                          iconSize: 32.0,
-                          onPressed: () {
-                            setState(() {
-                              images.removeAt(index);
-                            });
-                          },
-                          icon: new Icon(Icons.delete, color: Colors.red),
-                        ),
-                      ],
-                    )
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          new IconButton(
+                            iconSize: 32.0,
+                            onPressed: () {
+                              setState(() {
+                                images[index].rotation++;
+                              });
+                            },
+                            icon: new Icon(Icons.rotate_right, color: Theme
+                                .of(context)
+                                .primaryColor),
+                          ),
+                          new IconButton(
+                            iconSize: 32.0,
+                            onPressed: () {
+                              setState(() {
+                                images[index].rotation--;
+                              });
+                            },
+                            icon: new Icon(Icons.rotate_left, color: Theme
+                                .of(context)
+                                .primaryColor),
+                          ),
+                          new IconButton(
+                            iconSize: 32.0,
+                            onPressed: () {
+                              setState(() {
+                                images.removeAt(index);
+                              });
+                            },
+                            icon: new Icon(Icons.delete, color: Colors.red),
+                          ),
+                        ])
                   ]),
                 ),
               ]),
             );
           }),
+      bottomNavigationBar: new BottomAppBar(
+        color: Theme
+            .of(context)
+            .primaryColor,
+        hasNotch: true,
+        child: new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              new IconButton(
+                onPressed: () => _onImageButtonPressed(ImageSource.gallery),
+                tooltip: 'Add photo from gallery',
+                icon: new Icon(Icons.add_photo_alternate),
+              ),
+              new SizedBox(width: 20.0),
+              new IconButton(
+                onPressed: () => _onImageButtonPressed(ImageSource.camera),
+                tooltip: 'Add photo from camera',
+                icon: new Icon(Icons.add_a_photo),
+              ),
+            ]),
+      ),
+      floatingActionButton: (images.length > 0) ? new FloatingActionButton(
+        onPressed: () => _onSendButtonPressed(),
+        tooltip: 'Send to RevSpace wiki',
+        child: new Icon(Icons.send),
+      ) : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
     );
   }
+
 
   void _onSendButtonPressed() {
     Navigator.of(context).push(
@@ -193,6 +265,66 @@ class SelectPhotosState extends State<SelectPhotos> {
               );
             }
         )
+    );
+  }
+
+  void _onSettingsButtonPressed() {
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                    title: new Text('RevSpace Settings'),
+                  ),
+                  body: new SingleChildScrollView(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const SizedBox(height: 24.0),
+                          new Row(
+                              children: [
+                                const Expanded(
+                                  child: const Text('This account should be your revSpace Wiki account.\n\n'
+                                      'If you don\'t already have a wiki account, please contact a board member.'),
+                                ),
+                                new FloatingActionButton(
+                                    child: const Icon(Icons.email),
+                                    tooltip: 'Send email to board',
+                                    mini: true,
+                                    onPressed: () {
+                                      launch('mailto:board@revspace.nl?subject=Nieuw wiki account&body='
+                                          'Beste Bestuur,\n\n'
+                                          'Graag zou ik een account krijgen voor de RevSpace Wiki.\n\n'
+                                          'Mijn nickname is: \n\n'
+                                          'Geautomatiseerde groeten,\n\n'
+                                          'De RevSpace App');
+                                    }),
+                              ]),
+                          const SizedBox(height: 24.0),
+                          new TextFormField(
+                            decoration: const InputDecoration(
+                              border: const UnderlineInputBorder(),
+                              filled: true,
+                              hintText: 'RevSpace Wiki username',
+                              labelText: 'Username',
+                            ),
+                            onSaved: (String value) {
+                              print('Name: $value');
+                            },
+                          ),
+                          const SizedBox(height: 24.0),
+                          new PasswordField(
+                            hintText: 'RevSpace Wiki password',
+                            labelText: 'Password',
+                            onSaved: (String value) {
+                              print('Password: $value');
+                            },
+                          ),
+                        ]),
+                  )
+              );
+            })
     );
   }
 
