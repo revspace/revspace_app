338add3b56e96ce720a7d98d748b5de31a0e1d83
Peetz0r <github@haas-en-berg.nl>
2018-05-16 05:11:39 +0200

    second commit
    yay?

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,13 @@ import 'package:image_picker/image_picker.dart';
 void main() => runApp(new MyApp());
 
 List<RevImage> images = new List<RevImage>();
+MyAppState myAppState;
 
 class MyApp extends StatefulWidget {
   @override
   MyAppState createState() {
-    return new MyAppState();
+    myAppState = new MyAppState();
+    return myAppState;
   }
 }
 
@@ -26,9 +28,11 @@ class RevImage {
 
 class RevFileImage extends FileImage {
   RevImage _ri;
+  BuildContext _context;
 
-  RevFileImage(RevImage ri) : super(ri.file) {
+  RevFileImage(RevImage ri, BuildContext context) : super(ri.file) {
     _ri = ri;
+    _context = context;
   }
 
   @override
@@ -51,79 +55,19 @@ class RevFileImage extends FileImage {
     try {
       imageCodec = await ui.instantiateImageCodec(bytes);
     } catch (e) {
-      images.remove(_ri);
+      myAppState.setState(() {
+        images.remove(_ri);
+        Scaffold.of(_context).showSnackBar(new SnackBar(content: new Text('That is not a valid photo.')));
+      });
+      // We need to clear the cache because if we don't and the user selects the same invalid photo twice
+      // then the photo won't ever load using this method and we won't catch the exception.
+      PaintingBinding.instance.imageCache.clear();
+      // I need to provide some valid image data here so it can continue living before the ListView.builder removes the item.
+      // If we don't we will just have the exception we are trying to handle again.
       // @formatter:off
       // stolen from https://github.com/shinnn/node-smallest-png/blob/master/index.js
-      Uint8List dummy = new Uint8List.fromList([
-        137,
-        80,
-        78,
-        71,
-        13,
-        10,
-        26,
-        10,
-        0,
-        0,
-        0,
-        13,
-        73,
-        72,
-        68,
-        82,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        1,
-        8,
-        6,
-        0,
-        0,
-        0,
-        31,
-        21,
-        196,
-        137,
-        0,
-        0,
-        0,
-        10,
-        73,
-        68,
-        65,
-        84,
-        120,
-        156,
-        99,
-        0,
-        1,
-        0,
-        0,
-        5,
-        0,
-        1,
-        13,
-        10,
-        45,
-        180,
-        0,
-        0,
-        0,
-        0,
-        73,
-        69,
-        78,
-        68,
-        174,
-        66,
-        96,
-        130
-      ]);
-      // @formatter:on
+      Uint8List dummy = new Uint8List.fromList([137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 8, 6, 0, 0, 0, 31, 21, 196, 137, 0, 0, 0, 10, 73, 68, 65, 84, 120, 156, 99, 0, 1, 0, 0, 5, 0, 1, 13, 10, 45, 180, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130]);
+      // @formatter:on      
       imageCodec = await ui.instantiateImageCodec(dummy);
     }
     return imageCodec;
@@ -131,18 +75,11 @@ class RevFileImage extends FileImage {
 }
 
 class MyAppState extends State<MyApp> {
-  final myController = new TextEditingController();
 
-  @override
-  void dispose() {
-    // Clean up the controller when the Widget is disposed
-    myController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final title = 'Basic List';
+    final title = 'Select photos';
 
     return new MaterialApp(
       title: title,
@@ -162,7 +99,7 @@ class MyAppState extends State<MyApp> {
               icon: new Icon(Icons.add_a_photo),
             ),
             new IconButton(
-              onPressed: () => _onSendButtonPressed,
+              onPressed: () => _onSendButtonPressed(),
               tooltip: 'Send to RevSpace wiki',
               color: (images.length > 0) ? Colors.black : Colors.grey,
               icon: new Icon(Icons.send),
@@ -179,7 +116,7 @@ class MyAppState extends State<MyApp> {
                     quarterTurns: images[index].rotation,
                     child: new FadeInImage(
                         placeholder: new AssetImage('assets/loading.gif'),
-                        image: new RevFileImage(images[index]),
+                        image: new RevFileImage(images[index], context),
                         width: 144.0,
                         height: 144.0,
                         fit: BoxFit.cover),
@@ -204,7 +141,9 @@ class MyAppState extends State<MyApp> {
                                 images[index].rotation++;
                               });
                             },
-                            icon: new Icon(Icons.rotate_right, color: Theme.of(context).primaryColor),
+                            icon: new Icon(Icons.rotate_right, color: Theme
+                                .of(context)
+                                .primaryColor),
                           ),
                           new IconButton(
                             iconSize: 32.0,
@@ -213,7 +152,9 @@ class MyAppState extends State<MyApp> {
                                 images[index].rotation--;
                               });
                             },
-                            icon: new Icon(Icons.rotate_left, color: Theme.of(context).primaryColor),
+                            icon: new Icon(Icons.rotate_left, color: Theme
+                                .of(context)
+                                .primaryColor),
                           ),
                           new IconButton(
                             iconSize: 32.0,
@@ -237,6 +178,18 @@ class MyAppState extends State<MyApp> {
 
   void _onSendButtonPressed() {
     print('Send button pressed');
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                appBar: new AppBar(
+                  title: new Text('Send'),
+                ),
+                body: new CircularProgressIndicator(),
+              );
+            }
+        )
+    );
   }
 
   void _onImageButtonPressed(ImageSource source) {
